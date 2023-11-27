1c39e025673bfbcd31f4292caa7e3ba86680323b
Cody <qtipcody@gmail.com>
2023-11-27 15:20:35 -0700

    setup

---
+++ Controllers/DogsController.cs
@@ -0,0 +1,11 @@
+namespace dog_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class DogsController : ControllerBase
+{
+
+
+
+}
---
+++ Models/Dog.cs
@@ -0,0 +1,21 @@
+
+
+namespace dog_api.Models;
+
+public class Dog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool LikesToys { get; set; }
+    public int NumberOfLegs { get; set; }
+    public Dog(int id, string name, int age, bool likesToys, int Color)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        LikesToys = likesToys;
+        NumberOfLegs = NumberOfLegs;
+    }
+}
+
